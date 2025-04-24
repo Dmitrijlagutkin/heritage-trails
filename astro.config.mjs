a8c9dad53e5ad739160d055dd51494a0974ac024
Dmitrijlagutkin <dmitrijlagutkin@gmail.com>
2025-04-24 17:45:41 +0200

    feat: translations, home page structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import i18n from "@astrolicious/i18n";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [
+    react(),
+    i18n({
+      defaultLocale: "en",
+      locales: ["en", "es", "ua"], // must include the default locale
+      client: {
+        data: true,
+      },
+    }),
+  ],
 });
--- src/components/MapSection.tsx
+++ src/components/MapSection.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export const MapSection = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="map-section">
-      <h2>Map Section</h2>
+    <div className="container">
+      <h2>Second Section</h2>
       <button onClick={() => setCount(count + 1)}>Count: {count}</button>
     </div>
   );
