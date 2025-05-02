988155676bba4e2a2acf46b46f0d6fe37da025e4
Dmitrijlagutkin <dmitrijlagutkin@gmail.com>
2025-05-02 09:41:33 +0200

    fix: remove translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import i18n from "@astrolicious/i18n";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    i18n({
-      defaultLocale: "en",
-      locales: ["en", "es", "ua"], // must include the default locale
-      client: {
-        data: true,
-      },
-    }),
-  ],
+  integrations: [react()],
 
   vite: {
     plugins: [tailwindcss()],
