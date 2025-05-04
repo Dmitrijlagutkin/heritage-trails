1d00d3028d657ca7bfb90ac68b03ebfb029cb553
Dmitrijlagutkin <dmitrijlagutkin@gmail.com>
2025-05-04 18:41:27 +0200

    refactor: data folder

---
+++ src/features/features-section/data/features.ts
@@ -0,0 +1,14 @@
+export const features = [
+    {
+        title: 'home.features_section.item_1_title',
+        description: 'home.features_section.item_1_description'
+    },
+    {
+        title: 'home.features_section.item_2_title',
+        description: 'home.features_section.item_2_description'
+    },
+    {
+        title: 'home.features_section.item_3_title',
+        description: 'home.features_section.item_3_description'
+    }
+];
---
+++ src/features/statistic-section/data/statisticList.ts
@@ -0,0 +1,8 @@
+export const statisticList = [
+    { title: "National Parks and Preserves", number: "70+" },
+    { title: "Historical Sites and Memorials", number: "200+" },
+    { title: "Recreational Areas", number: "30+" },
+    { title: "Special Designations", number: "85+" },
+    { title: "Trails and Parkways", number: "10+" },
+    { title: "Waterways and Rivers", number: "10+" }
+];
